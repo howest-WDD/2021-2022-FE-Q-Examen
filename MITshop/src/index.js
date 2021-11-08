dbc4164813740e02f895e6df8e61ccea21cd6fb6
Lien Stevens <lien.stevens@howest.be>
2021-11-08 13:48:16 +0100

    Update index.js

--- MITshop/src/index.js
+++ MITshop/src/index.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ShopApp from "./ShopApp";
-import "./styles.scss";
-
-ReactDOM.render(
-  <ShopApp />,
-  document.getElementById('root')
-);
 

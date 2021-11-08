a6c0cbced6b77657b25fd10f30c79a207754e43c
Lien Stevens <lien.stevens@howest.be>
2021-11-08 13:47:53 +0100

    Update ArticlesList.js

--- MITshop/src/ArticlesList.js
+++ MITshop/src/ArticlesList.js
@@ -1,15 +1,1 @@
-import React from "react";
-import Article from "./Article";
 
-function ArticlesList(props) {
-    return (
-        <>
-            <h2 className="c-winkelmandje">Winkelmandje</h2>
-            {props.articles.map((article) => (
-                <Article key={article.article_id} article={article} deleteArticleProps={props.deleteArticleProps} />
-            ))}
-        </>
-    );
-}
-
-export default ArticlesList;
